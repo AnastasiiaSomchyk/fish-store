c67815ec531eeb94bfa5284810ea0742697a23cb
Anastasiia Somchyk <e4nastya@gmail.com>
2018-05-01 19:48:43 -0500

    events

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,9 +1,11 @@
 const loadFishes = require('./fishes');
 const writeFishes = require('./fishDom');
+const binEvents = require('./events');
 
 const whenFishesLoad = (data) => {
   console.log('data', data);
   $('#available').append(writeFishes(data.fishes));
+  binEvents();
 };
 
 const whenFishesDontLoad = (error) => {
